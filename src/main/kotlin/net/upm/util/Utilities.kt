c8252d87439239dcb8f690f0c20251a4e24e4f53
Pavan P <paramvir_phagura@outlook.com>
2021-06-30 20:45:22 -0700

    Update chooseDatabase
    Last accessed directory includes initial database directory.

--- src/main/kotlin/net/upm/util/Utilities.kt
+++ src/main/kotlin/net/upm/util/Utilities.kt
@@ -2,13 +2,23 @@ package net.upm.util
 
 import javafx.stage.FileChooser
 import javafx.stage.Stage
+import net.upm.util.config.UserConfiguration
 import java.awt.Desktop
 import java.io.File
 import java.net.URI
 import java.nio.file.Path
+import java.nio.file.Paths
 
 val USER_HOME: String = System.getProperty("user.home")
 val USER_HOME_FILE = File(USER_HOME)
+
+val initialDatabaseDirectory =
+    if (UserConfiguration.INSTANCE.initialDatabase.value.isEmpty())
+        null
+    else
+        Paths.get(
+            UserConfiguration.INSTANCE.initialDatabase.value.substring(0,
+                UserConfiguration.INSTANCE.initialDatabase.value.lastIndexOf("\\")))
 
 /**
  * The previously accessed directory.
@@ -17,11 +27,10 @@ var previousDir: Path? = null
 val previousDirFile: File?
     get() = previousDir?.toFile()
 
-fun chooseDatabase(owner: Stage): File?
-{
+fun chooseDatabase(owner: Stage): File? {
     val fileChooser = FileChooser()
     fileChooser.title = "Select an account database"
-    fileChooser.initialDirectory = previousDirFile ?: USER_HOME_FILE
+    fileChooser.initialDirectory = previousDirFile ?: initialDatabaseDirectory?.toFile() ?: USER_HOME_FILE
     fileChooser.extensionFilters.addAll(FileChooser.ExtensionFilter("Account DB", "*", "*.adb"))
 
     val file = fileChooser.showOpenDialog(owner)
@@ -30,17 +39,14 @@ fun chooseDatabase(owner: Stage): File?
     return file
 }
 
-fun openUrl(url: String)
-{
+fun openUrl(url: String) {
     if (!url.contains("http"))
         return
 
-    try
-    {
+    try {
         if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE))
             Desktop.getDesktop().browse(URI.create(url))
-    } catch(e: Exception)
-    {
+    } catch (e: Exception) {
         e.printStackTrace()
     }
 }
